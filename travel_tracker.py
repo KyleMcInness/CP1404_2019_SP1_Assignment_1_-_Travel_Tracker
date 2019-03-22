6070ac6904a0075943d6342506e935c14f64f2b4
Kyle <ktmcinness@gmail.com>
2019-03-23 09:57:38 +1000

    fix menu and add fucntions

--- travel_tracker.py
+++ travel_tracker.py
@@ -10,9 +10,10 @@ M - Mark a place as visited
 Q - Quit
 > """
 
-MENU_CHOICE = "LAMlam"
+MENU_CHOICE = "LAMQ"
 PLACES = list_of_places.read().split("\n")
-additions = []
+print(PLACES)
+
 
 def main():
     print("Welcome, my name is Kyle.")
@@ -22,36 +23,50 @@ def main():
         print("That was an invalid choice")
         menu_choice = input(MENU)
 
-    while menu_choice != "Q" or menu_choice != "q":
+    while menu_choice != "Q":
         if menu_choice == "L" or menu_choice == "l":
-            for position in range(len(PLACES)):
-                if PLACES[position].split(",")[-1] == "n":
-                    print("*{}. {} in {} priority {}".format(position + 1, PLACES[position].split(',')[0], PLACES[position].split(',')[1], PLACES[position].split(',')[2]))
-                else:
-                    print("{}. {} in {} priority {}".format(position + 1, PLACES[position].split(',')[0],
-                                                             PLACES[position].split(',')[1],
-                                                             PLACES[position].split(',')[2]))
+
+            display_places()
             menu_choice = input(MENU)
         elif menu_choice == "A" or menu_choice == "a":
-            place_name = input("Name: ")
-            additions.append(place_name)
-            country_name = input("Country: ")
-            additions.append(country_name)
-            priority_value = input("Priority: ")
-            additions.append(priority_value)
-            additions.append("n")
-            print("{} in {} (priority {}) has been added to Travel Tracker".format(place_name, country_name, priority_value))
-            print(additions)
+            add_new_place()
             menu_choice = input(MENU)
-        else:
-            '''tbc'''
+        elif menu_choice == "M" or menu_choice == "m":
+            print('''tbc''')
 
     print("Bye")
-    for position in additions:
+    for position in PLACES:
+        list_of_places.write("\n")
         list_of_places.write(position)
+    list_of_places.close()
 
 
-list_of_places.close()
+def display_places():
+    unvisited = 0
+    visited = 0
+    for position in range(len(PLACES)):
+        if PLACES[position].split(",")[-1] == "n":
+            print("*{}. {} in {} priority {}".format(position + 1, PLACES[position].split(',')[0],
+                                                     PLACES[position].split(',')[1], PLACES[position].split(',')[2]))
+            unvisited += 1
+        else:
+            print("{}. {} in {} priority {}".format(position + 1, PLACES[position].split(',')[0],
+                                                    PLACES[position].split(',')[1], PLACES[position].split(',')[2]))
+            visited += 1
+    print("{} place(s). You still want to visit {} place(s)".format(unvisited + visited, unvisited))
+
+
+def add_new_place():
+    new_place = ""
+    place_name = input("Name: ")
+    new_place += place_name + ","
+    country_name = input("Country: ")
+    new_place += country_name + ","
+    priority_value = input("Priority: ")
+    new_place += priority_value + ","
+    new_place += "n"
+    PLACES.append(new_place)
+    print("{} in {} (priority {}) has been added to Travel Tracker".format(place_name, country_name, priority_value))
 
 
 main()
