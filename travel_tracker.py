42a3cf4fdbd7574294cda44c5ed8c0482e7c9112
Kyle <ktmcinness@gmail.com>
2019-03-23 11:29:07 +1000

    update mark visited update writing to file

--- travel_tracker.py
+++ travel_tracker.py
@@ -1,7 +1,7 @@
 """Kyle McInness, 23/03/19, Travel Tracker,
 https://github.com/KyleMcInness/CP1404_2019_SP1_Assignment_1_-_Travel_Tracker"""
 
-list_of_places = open("list_of_places.txt", 'r+')
+input_file = open("list_of_places.txt", 'r')
 
 MENU = """Menu:
 L - List of places
@@ -11,8 +11,11 @@ Q - Quit
 > """
 
 MENU_CHOICE = "LAMQ"
-PLACES = list_of_places.read().split("\n")
-print(PLACES)
+PLACES = input_file.read().split("\n")
+new_places = []
+for pos in PLACES:
+    new_places.append(pos)
+print(new_places)
 
 
 def main():
@@ -25,35 +28,53 @@ def main():
 
     while menu_choice != "Q":
         if menu_choice == "L" or menu_choice == "l":
-
             display_places()
             menu_choice = input(MENU)
         elif menu_choice == "A" or menu_choice == "a":
             add_new_place()
             menu_choice = input(MENU)
         elif menu_choice == "M" or menu_choice == "m":
-            print('''tbc''')
-
+            display_places()
+            mark_visited()
+            menu_choice = input(MENU)
     print("Bye")
-    for position in PLACES:
-        list_of_places.write("\n")
-        list_of_places.write(position)
-    list_of_places.close()
+    output_file = open("list_of_places.txt", "w")
+    for position in new_places:
+        output_file.write(position + "\n")
+    input_file.close()
+    output_file.close()
+
+
+def mark_visited():
+    number_of_place = int(input("Enter the number of a place to mark as visited "
+                             "> "))
+    print("{} in {} visited.".format(PLACES[number_of_place - 1].split(",")[0], PLACES[number_of_place - 1].split(",")[1]))
+    visited_place = new_places[number_of_place - 1]
+    visited_places = visited_place.split(",")
+    new_entry = ""
+    for i in range(len(visited_places) -1):
+        new_entry += visited_places[i]
+        new_entry += ","
+    new_entry += "v"
+    new_places[number_of_place - 1] = new_entry
 
 
 def display_places():
     unvisited = 0
     visited = 0
-    for position in range(len(PLACES)):
-        if PLACES[position].split(",")[-1] == "n":
-            print("*{}. {} in {} priority {}".format(position + 1, PLACES[position].split(',')[0],
-                                                     PLACES[position].split(',')[1], PLACES[position].split(',')[2]))
-            unvisited += 1
-        else:
-            print("{}. {} in {} priority {}".format(position + 1, PLACES[position].split(',')[0],
-                                                    PLACES[position].split(',')[1], PLACES[position].split(',')[2]))
-            visited += 1
-    print("{} place(s). You still want to visit {} place(s)".format(unvisited + visited, unvisited))
+    try:
+        for position in range(len(new_places)):
+            if new_places[position].split(",")[-1] == "n":
+                print("*{}. {} in {} priority {}".format(position + 1, new_places[position].split(',')[0],
+                                                         new_places[position].split(',')[1], new_places[position].split(',')[2]))
+                unvisited += 1
+            else:
+                print("{}. {} in {} priority {}".format(position + 1, new_places[position].split(',')[0],
+                                                        new_places[position].split(',')[1], new_places[position].split(',')[2]))
+                visited += 1
+        print("{} place(s). You still want to visit {} place(s)".format(unvisited + visited, unvisited))
+    except IndexError:
+        pass
 
 
 def add_new_place():
@@ -65,7 +86,7 @@ def add_new_place():
     priority_value = input("Priority: ")
     new_place += priority_value + ","
     new_place += "n"
-    PLACES.append(new_place)
+    new_places.append(new_place)
     print("{} in {} (priority {}) has been added to Travel Tracker".format(place_name, country_name, priority_value))
 
 
